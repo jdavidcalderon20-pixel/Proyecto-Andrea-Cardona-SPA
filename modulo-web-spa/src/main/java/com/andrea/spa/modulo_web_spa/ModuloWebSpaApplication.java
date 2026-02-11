36cd85a617776fadc8cdc93888e12cc186a81c38
jdavdicalderon20-pixel <jdavidcalderon20@gmail.com>
2026-02-10 21:31:58 -0500

    Migración a Framework Spring Boot - Evidencia AA3

---
+++ modulo-web-spa/src/main/java/com/andrea/spa/modulo_web_spa/ModuloWebSpaApplication.java
@@ -0,0 +1,13 @@
+package com.andrea.spa.modulo_web_spa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuloWebSpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuloWebSpaApplication.class, args);
+	}
+
+}
---
+++ modulo-web-spa/src/test/java/com/andrea/spa/modulo_web_spa/ModuloWebSpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrea.spa.modulo_web_spa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuloWebSpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
