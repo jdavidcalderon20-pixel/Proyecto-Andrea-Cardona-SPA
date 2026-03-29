f727f9bb0cfb0e7fd1a0956d39df7637751553fa
jdavdicalderon20-pixel <jdavidcalderon20@gmail.com>
2026-03-28 19:16:51 -0500

    security: relocate firebase credentials to environment variables and rotate api key

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyB26FSx9mybYC0tp8XEnvD-T4PpF2qCMB4",
-  authDomain: "andreacardonaspa.firebaseapp.com",
-  projectId: "andreacardonaspa",
-  storageBucket: "andreacardonaspa.firebasestorage.app",
-  messagingSenderId: "218605189150",
-  appId: "1:218605189150:web:13a1dcd513cdba8829960f",
-  measurementId: "G-MWM47FK07Y"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
